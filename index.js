942c6c633498a12dbaece8648228a45cb5d6d2c9
edithk7 <edithkeeler7@gmail.com>
2016-09-22 22:18:20 +0300

    Fixed showing title bar on windows

--- index.js
+++ index.js
@@ -5,41 +5,17 @@ var remote = require('electron').remote;
 var electronImageResize = require('electron-image-resize');
 var BrowserWindow = remote.BrowserWindow;
 
-// build titlebar if not on linux
-if (remote.process.platform != "linux") {
-  var title_bar = document.createElement("div");
-  var title = document.createElement("div");
-  var title_bar_btns = document.createElement("div");
-  var min_btn = document.createElement("button");
-  var max_btn = document.createElement("button");
-  var close_btn = document.createElement("button");
-
-  title_bar.setAttribute("id", "title-bar");
-  title.setAttribute("id", "title");
-  title_bar_btns.setAttribute("id", "title-bar-btns");
-  min_btn.setAttribute("id", "min-btn");
-  min_btn.setAttribute("class", "button");
-  max_btn.setAttribute("id", "max-btn");
-  max_btn.setAttribute("class", "button");
-  close_btn.setAttribute("id", "close-btn");
-  close_btn.setAttribute("class", "button");
-
-  title_bar_btns.appendChild(min_btn);
-  title_bar_btns.appendChild(max_btn);
-  title_bar_btns.appendChild(close_btn);
-
-  title_bar.appendChild(title);
-  title_bar.appendChild(title_bar_btns);
-
-  document.body.appendChild(title_bar);
-}
-
 var moviesList = [];
 var magnetLinks = {};
 var moviesYears = {};
 var moviesIds = {};
 var movieYear = 0;
 var deletedMoviesFile = path.join(__dirname, "cache/deletedMovies.txt");
+
+// if on linux, hide title bar
+if (remote.platform == "linux") {
+  $("#title-bar").remove();
+}
 
 loadMovies();
 
@@ -128,77 +104,77 @@ function fillMoviePoster(movieName, id) {
           rig_text.style.fontSize = "14px";
         }
 
-        rig_text.innerHTML = movieInfoText;        
+        rig_text.innerHTML = movieInfoText;
         rig_text.addEventListener('click', function() {
-        openImdb(movieInfo["imdbID"]);
-      });
-
-
-      var deleteButton = document.createElement("input");
-      var downloadButton = document.createElement("input");
-
-      deleteButton.setAttribute("type", "image");
-      deleteButton.src = path.join(__dirname, "img/delete.png");
-      deleteButton.setAttribute("height", "50px");
-      deleteButton.setAttribute("width", "50px");
-      deleteButton.setAttribute("class", "delete_button");
-      deleteButton.addEventListener('click', function() {
-        deleteMovie(movieName);
-      });
-
-      downloadButton.setAttribute("type", "image");
-      downloadButton.src = path.join(__dirname, "img/download.png");
-      downloadButton.setAttribute("height", "50px");
-      downloadButton.setAttribute("width", "50px");
-      downloadButton.setAttribute("class", "download_button");
-      downloadButton.addEventListener('click', function() {
-        downloadMovie(movieName);
-      });
-
-      rig_overlay.appendChild(downloadButton);
-      rig_overlay.appendChild(deleteButton);
-
-      if (movieInfo["Poster"] != "N/A") {
-        var cachedImage = path.join(__dirname, "cache/" + movieName + '.jpg');
-
-        // download image if needed
-        fs.stat(cachedImage, function(err, stat) {
-          if(err != null) {
-            rig_img.src = movieInfo['Poster'];
-            electronImageResize({
-              url: movieInfo['Poster'],
-              width: 300,
-              height: 445
-            }).then(img => {
-              fs.writeFile(cachedImage, img.toJpeg(100), (err) => {
-                if (err) throw err;
-              });
-            })
-          }
-          else {
-            rig_img.src = cachedImage;
-          }
+          openImdb(movieInfo["imdbID"]);
         });
+
+
+        var deleteButton = document.createElement("input");
+        var downloadButton = document.createElement("input");
+
+        deleteButton.setAttribute("type", "image");
+        deleteButton.src = path.join(__dirname, "img/delete.png");
+        deleteButton.setAttribute("height", "50px");
+        deleteButton.setAttribute("width", "50px");
+        deleteButton.setAttribute("class", "delete_button");
+        deleteButton.addEventListener('click', function() {
+          deleteMovie(movieName);
+        });
+
+        downloadButton.setAttribute("type", "image");
+        downloadButton.src = path.join(__dirname, "img/download.png");
+        downloadButton.setAttribute("height", "50px");
+        downloadButton.setAttribute("width", "50px");
+        downloadButton.setAttribute("class", "download_button");
+        downloadButton.addEventListener('click', function() {
+          downloadMovie(movieName);
+        });
+
+        rig_overlay.appendChild(downloadButton);
+        rig_overlay.appendChild(deleteButton);
+
+        if (movieInfo["Poster"] != "N/A") {
+          var cachedImage = path.join(__dirname, "cache/" + movieName + '.jpg');
+
+          // download image if needed
+          fs.stat(cachedImage, function(err, stat) {
+            if(err != null) {
+              rig_img.src = movieInfo['Poster'];
+              electronImageResize({
+                url: movieInfo['Poster'],
+                width: 300,
+                height: 445
+              }).then(img => {
+                fs.writeFile(cachedImage, img.toJpeg(100), (err) => {
+                  if (err) throw err;
+                });
+              })
+            }
+            else {
+              rig_img.src = cachedImage;
+            }
+          });
+        }
+        else {
+          rig_img.src = path.join(__dirname, "img/NA.jpg");
+        }
+
+        // After all info is ready, present table
+        if (id == moviesList.length - 1) {
+          $("#loading-img").fadeOut(500);
+          $("#rig").fadeIn(1000);
+        }
       }
       else {
-        rig_img.src = path.join(__dirname, "img/NA.jpg");
+        alert("error");
       }
-
-      // After all info is ready, present table
-      if (id == moviesList.length - 1) {
-        $("#loading-img").fadeOut(500);
-        $("#rig").fadeIn(1000);
-      }
-    }
-    else {
-      alert("error");
     }
   }
-}
-var movieSearchString = movieName.replace(/\s/g, '+');
-movieSearchString = "http://www.omdbapi.com/?t="+movieSearchString+"&y="+moviesYears[movieName]+"&plot=short&r=json";
-jsonhttp.open("GET", movieSearchString, true);
-jsonhttp.send();
+  var movieSearchString = movieName.replace(/\s/g, '+');
+  movieSearchString = "http://www.omdbapi.com/?t="+movieSearchString+"&y="+moviesYears[movieName]+"&plot=short&r=json";
+  jsonhttp.open("GET", movieSearchString, true);
+  jsonhttp.send();
 }
 
 function getMovieName(movieString) {
@@ -241,34 +217,32 @@ function openImdb(id) {
 }
 
 // Bind delete/download/min/max/close buttons
-if (remote.process.platform != "linux") {
-  (function () {
-    function init() {
-      document.getElementById("min-btn").addEventListener("click", function (e) {
-        var window = BrowserWindow.getFocusedWindow();
-        window.minimize();
-      });
+(function () {
+  function init() {
+    document.getElementById("min-btn").addEventListener("click", function (e) {
+      var window = BrowserWindow.getFocusedWindow();
+      window.minimize();
+    });
 
-      document.getElementById("max-btn").addEventListener("click", function (e) {
-        var window = BrowserWindow.getFocusedWindow();
-        if (!window.isMaximized()) {
-          window.maximize();
-        } else {
-          window.unmaximize();
-        }
-      });
-
-      document.getElementById("close-btn").addEventListener("click", function (e) {
-        var window = BrowserWindow.getFocusedWindow();
-        window.close();
-      });
-    };
-
-
-    document.onreadystatechange = function () {
-      if (document.readyState == "complete") {
-        init();
+    document.getElementById("max-btn").addEventListener("click", function (e) {
+      var window = BrowserWindow.getFocusedWindow();
+      if (!window.isMaximized()) {
+        window.maximize();
+      } else {
+        window.unmaximize();
       }
-    };
-  })();
-}
+    });
+
+    document.getElementById("close-btn").addEventListener("click", function (e) {
+      var window = BrowserWindow.getFocusedWindow();
+      window.close();
+    });
+  };
+
+
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      init();
+    }
+  };
+})();
