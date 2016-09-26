eb8954e1d6d07480d5dab037b636cd6d2b6f0762
edithk7 <edithkeeler7@gmail.com>
2016-09-26 07:48:16 +0300

    close fix

--- main.js
+++ main.js
@@ -52,7 +52,12 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
-    app.quit()
+    try {
+      app.quit()
+    }
+    catch (err){
+      return
+    }
   })
 }
 
@@ -63,11 +68,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
     app.quit()
-  }
 })
 
 app.on('activate', () => {
